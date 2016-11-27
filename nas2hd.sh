3760b9f143718efcb697b84b8b687735d7a09f81
Morgan Jones <morgan@larry-2.local>
2016-11-26 22:06:41 -0500

    now chooses rsync direction based on script name, mtns-nas hardcoded

--- nas2hd.sh
+++ nas2hd.sh
@@ -10,25 +10,40 @@
 #cmd="rsync3 00cmdline00 --iconv=utf-8-mac,utf-8 --exclude .Apple* --progress --delete -avHe ssh nas:/00dir00/ /Volumes/00dest00/00dir00"
 # cmd="rsync3 00cmdline00 --iconv=utf-8-mac,utf-8 --exclude .Apple* --progress --delete -avHe ssh"
 # if [ $1 == "-r" ]; then
-# else 
+# else
+
+name=`basename $0`
 
 for dest in seagate wd; do
 #echo; echo
 #for dest in $dests; do
-    if [ -d "/Volumes/$dest" ]; then 
+    if [ -d "/Volumes/$dest" ]; then
+	
 	echo; echo
-	for dir in photos movies backup vmbackups; do 
-#	for dir in vmbackups; do 
+#	for dir in photos movies backup vmbackups; do
+	for dir in photos movies backup; do 
+	    #	for dir in vmbackups; do 
 	    # echo; echo ${dir}...
 	    # run_cmd=`echo $cmd|sed "s/00cmdline00/$*/" | sed "s/00dir00/$dir/g" | sed "s/00dest00/$dest/g"`
 	    # echo $run_cmd
             # $run_cmd
 
-#    rsync3 $* --iconv=utf-8-mac,utf-8 --exclude .Apple* --progress --delete -avHe ssh nas:/${dir}/ /Volumes/seagate\ horizontal/${dir}; 
-#    $cmd
-	cmd="rsync $* --iconv=utf-8-mac,utf-8 --exclude .Apple* --progress --delete -avHe ssh nas:/${dir}/ /Volumes/$dest/${dir}"
-	echo $cmd
-	$cmd
+	    #    rsync3 $* --iconv=utf-8-mac,utf-8 --exclude .Apple* --progress --delete -avHe ssh nas:/${dir}/ /Volumes/seagate\ horizontal/${dir}; 
+	    #    $cmd
+
+
+	    # cmd="rsync $* --iconv=utf-8-mac,utf-8 --exclude .Apple* --progress --delete -avHe ssh nas:/${dir}/ /Volumes/$dest/${dir}"
+	    #	    cmd="rsync $* --iconv=utf-8-mac,utf-8 --exclude .Apple* --progress --delete -avHe ssh "
+	    cmd="rsync $* --exclude ccc.sparsebundle --iconv=utf-8-mac,utf-8 --exclude .Apple* --progress -avHe ssh "
+
+
+	    if [ $name == "nas2hd.sh" ]; then
+		cmd=${cmd}"mtns-nas:/${dir}/ /Volumes/$dest/${dir}/"
+	    else
+		cmd=${cmd}"/Volumes/$dest/${dir}/ mtns-nas:/${dir}/"
+	    fi
+	    echo $cmd
+	    $cmd
 
 	done
     fi
